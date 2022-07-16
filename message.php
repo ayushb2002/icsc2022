ed2ba7119831ec0bd7a462e1ea7cb80ca1a92536
Ayush Bansal <ayushb6732@gmail.com>
2022-07-16 13:05:42 +0530

    General Chair image added

--- message.php
+++ message.php
@@ -20,19 +20,42 @@
         </section>
         <section>
             <div class="flex flex-col py-4 px-24">
-                <div class="flex justify-start p-2">
+                <div class="flex justify-center py-4">
                     <span class="m-5 text-3xl font-bold dark:">Message from General Chair</span>
                 </div>
                 <div></div>
-                <div class="col-span-4 p-5">
-                    <span class="text-xl font-bold">Prof. Shweta Srivastava</span> <br><br>
-                    <span class="text-sm">Professor and Head <br>General Chair, ICSC 2022</span>
+                <div class="lg:col-span-4 p-5">
+
+                    <div class="grid lg:grid-cols-2">
+                        <div class="">
+                        <span class="text-2xl font-bold">Prof. Shweta Srivastava</span> <br><br>
+                    <span class="text-md">Professor and Head <br>General Chair, ICSC 2022</span>
+                        </div>
+                        <div class="flex lg:justify-end justify-center">
+                            <img src="img/generalChair.jpg" alt="General Chair" class="w-[20vh] h-[20vh]">
+                        </div>
+                    </div>
+
                     <div class="divider"></div>
                     <p class="text-justify">
-                    I would like to take this opportunity to extend a warm greeting to you all on behalf of the Organizing Committee of the eighth edition of IEEE International Conference on Signal Processing and Communication (ICSC 2022). The conference will include an exceptional technical programme, which I am convinced will provide all the participants an engaging experience and stimulate exciting collaborative research. <br><br>
-The Eighth IEEE International Conference on Signal Processing and Communication (ICSC 2022) will offer an ideal international venue for the exchange of knowledge and findings in theory, methodology, and applications of signal and image processing and communication. The purpose of the conference is to provide researchers and practitioners working in industry and academic institutions the opportunity to interact with one another and discuss recent advances in relevant subjects. <br><br>
-Because of the unpredictability of the pandemic, we have chosen to have a virtual event, which has its own set of challenges, both in terms of technology and logistics. But the ease of attending the conference from anywhere in the world without involving any travel overcomes all other limitations. At our end, we have made every effort to ensure that the conference goes off without a hitch.
-We hope that throughout your time at the conference, you will not only be able to have a good time but also be successful in gaining knowledge about new research in your area.
+                        I would like to take this opportunity to extend a warm greeting to you all on behalf of the
+                        Organizing Committee of the eighth edition of IEEE International Conference on Signal Processing
+                        and Communication (ICSC 2022). The conference will include an exceptional technical programme,
+                        which I am convinced will provide all the participants an engaging experience and stimulate
+                        exciting collaborative research. <br><br>
+                        The Eighth IEEE International Conference on Signal Processing and Communication (ICSC 2022) will
+                        offer an ideal international venue for the exchange of knowledge and findings in theory,
+                        methodology, and applications of signal and image processing and communication. The purpose of
+                        the conference is to provide researchers and practitioners working in industry and academic
+                        institutions the opportunity to interact with one another and discuss recent advances in
+                        relevant subjects. <br><br>
+                        Because of the unpredictability of the pandemic, we have chosen to have a virtual event, which
+                        has its own set of challenges, both in terms of technology and logistics. But the ease of
+                        attending the conference from anywhere in the world without involving any travel overcomes all
+                        other limitations. At our end, we have made every effort to ensure that the conference goes off
+                        without a hitch.
+                        We hope that throughout your time at the conference, you will not only be able to have a good
+                        time but also be successful in gaining knowledge about new research in your area.
 
                     </p>
                 </div>
